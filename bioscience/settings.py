e477f60d1f917bd9f875e68d8842a56561529da9
Shudhanshu Gunjal <33764308+gshudhanshu@users.noreply.github.com>
2023-07-17 15:16:07 +0530

    hosts update

--- bioscience/settings.py
+++ bioscience/settings.py
@@ -26,7 +26,8 @@ SECRET_KEY = "django-insecure-@ycjtd=ko3=*a)2n(kb8hae9tprq!$)+n0+k^u%!^p5#1!e#0q
 DEBUG = True
 
 ALLOWED_HOSTS = [
-    ".sgcreative.pro"
+    ".sgcreative.pro",
+    "0.0.0.0",
     "localhost",
     "127.0.0.1",
     ".coursera-apps.org",
